3880161d6fea2e213124dd96e7361a06545a5918
Jacob Andersson <jacob.c.andersson1@gmail.com>
2023-02-01 07:50:23 +0100

    added cli

--- todo.md
+++ todo.md
@@ -1,6 +1,4 @@
 TODO:
-    create config class for readabilits
-    add cli to all parameters
     Track curve. Wandb or tensorboard
     Switch up the dataset and tokenization
     Add better resume logic
--- train.py
+++ train.py
@@ -1,92 +1,36 @@
 import numpy as np
 import torch
 import torch.nn as nn
-import model
+from model import GPT, ModelConfig
 import pickle
 import math
+import fire
+from dataclasses import dataclass
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-batch_size = 64
-block_size = 64
-num_epochs = 16
-n_layers = 10
-embedding_dim = 768
-internal_dim = 4*embedding_dim
-n_heads = 8
+@dataclass
+class BatchConfig:
+    batch_size: int
+    block_size: int
+    device: str
 
-learning_rate = 6e-5
-min_learning_rate = 6e-6
-warmup_iterations = 2000
-decay_iterations = 60000
-max_iterations = 60000
-testing_interval = 1000
-dropout = 0.1
+def get_batch(data, config):
+    rand = torch.randint(low=0, high=len(data) - config.block_size, size=(config.batch_size,))
 
-with open('meta.pkl', 'rb') as f:
-    pkl = pickle.load(f)
-    vocab_size = pkl['size']
-    char2idx = pkl['cti']
-    idx2char = pkl['itc']
+    x = torch.stack([torch.from_numpy((data[x: x+config.block_size]).astype(np.int64)) for x in rand])
+    y = torch.stack([torch.from_numpy((data[x+1: x+config.block_size+1]).astype(np.int64)) for x in rand])
 
-
-train_idx = np.memmap('train.bin', dtype=np.uint16)
-val_idx = np.memmap('test.bin', dtype=np.uint16)
-
-def get_batch(split='train'):
-    data = train_idx if split == 'train' else val_idx
-    rand = torch.randint(low=0, high=len(train_idx) - block_size, size=(batch_size,))
-
-    x = torch.stack([torch.from_numpy((data[x: x+block_size]).astype(np.int64)) for x in rand])
-    y = torch.stack([torch.from_numpy((data[x+1: x+block_size+1]).astype(np.int64)) for x in rand])
-
-    x = x.to(device)
-    y = y.to(device)
+    x = x.to(config.device)
+    y = y.to(config.device)
 
     return x,y
 
-config = model.ModelConfig(
-    vocab_size,
-    embedding_dim,
-    block_size,
-    n_layers,
-    internal_dim,
-    n_heads,
-    dropout,
-    device
-)
-
-model = model.GPT(config)
-model.to(device)
-print(model)
-
-prompt = 'hello thy'
-prompt_tokens = [char2idx[c] for c in prompt]
-
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-
-def get_learning_rate(iteration):
-    # Cosine decay with warmup. Think this is correct :) 
-    # https://keras.io/api/optimizers/learning_rate_schedules/cosine_decay/
-
-    if iteration < warmup_iterations:
-        return learning_rate * iteration/warmup_iterations
-    if iteration > decay_iterations:
-        return min_learning_rate
-
-    ratio = (iteration - warmup_iterations)/(decay_iterations - warmup_iterations)
-    cosine_decay = 0.5 * (1 + math.cos(math.pi*ratio))
-
-    return (learning_rate - min_learning_rate)*cosine_decay + min_learning_rate
-
-def test():
-    model.eval()
+def test(model, test_idx, batch_config):
     
     total_loss = 0.0
     counter = 0
 
     for i in range(200):
-        x, y = get_batch('test')
+        x, y = get_batch(test_idx, batch_config)
 
         y_pred = model(x)
         y_pred = y_pred.view(-1, y_pred.size(-1))
@@ -94,42 +38,112 @@ def test():
         total_loss += loss.item()
         counter += 1
 
-    model.train()
-
     return total_loss/counter
 
+def main(
+    device = 'cuda',
+    batch_size = 64,
+    block_size = 64,
+    num_epochs = 16,
+    n_layers = 10,
+    embedding_dim = 768,
+    n_heads = 8,
+    learning_rate = 6e-5,
+    min_learning_rate = 6e-6,
+    warmup_iterations = 2000,
+    decay_iterations = 60000,
+    max_iterations = 60000,
+    testing_interval = 1000,
+    dropout = 0.1,
+):
+    internal_dim = 4*embedding_dim
 
-counter = 0
-best_loss = float('inf')
+    with open('meta.pkl', 'rb') as f:
+        pkl = pickle.load(f)
+        vocab_size = pkl['size']
+        char2idx = pkl['cti']
+        idx2char = pkl['itc']
 
-while counter < max_iterations:
-    lr = get_learning_rate(counter)
-        
-    for param in optimizer.param_groups:
-        param['lr'] = lr
 
-    x, y = get_batch()
-    optimizer.zero_grad()
-    y_pred = model(x)
-    y_pred = y_pred.view(-1, y_pred.size(-1))
-    loss = criterion(y_pred, y.view(-1))
+    train_idx = np.memmap('train.bin', dtype=np.uint16)
+    val_idx = np.memmap('test.bin', dtype=np.uint16)
 
-    print("IDX: {}, loss: {}, lr: {}".format(counter, loss.item(), lr))
-    loss.backward()
-    optimizer.step()
+    config = ModelConfig(
+        vocab_size,
+        embedding_dim,
+        block_size,
+        n_layers,
+        internal_dim,
+        n_heads,
+        dropout,
+        device
+    )
+    print(config)
 
-    if counter and counter%testing_interval== 0:
-        checkpoint = model.state_dict()
+    batch_config = BatchConfig(batch_size, block_size, device)
+    print(batch_config)
 
-        test_loss = test()
-        print('test loss', test_loss)
+    def get_learning_rate(iteration):
+        # Cosine decay with warmup. Think this is correct :) 
+        # https://keras.io/api/optimizers/learning_rate_schedules/cosine_decay/
 
-        if test_loss < best_loss:
-            print('Improved best loss. Saving checkpoint')
-            best_loss = test_loss
-            torch.save(checkpoint, 'checkpoint.pt')
+        if iteration < warmup_iterations:
+            return learning_rate * iteration/warmup_iterations
+        if iteration > decay_iterations:
+            return min_learning_rate
 
-        out = model.generate(prompt_tokens, 100, char2idx, idx2char)
-        print(out)
+        ratio = (iteration - warmup_iterations)/(decay_iterations - warmup_iterations)
+        cosine_decay = 0.5 * (1 + math.cos(math.pi*ratio))
 
-    counter += 1
+        return (learning_rate - min_learning_rate)*cosine_decay + min_learning_rate
+
+
+    model = GPT(config)
+    model.to(device)
+    print(model)
+
+    prompt = 'hello thy'
+    prompt_tokens = [char2idx[c] for c in prompt]
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+    counter = 0
+    best_loss = float('inf')
+
+    while counter < max_iterations:
+        lr = get_learning_rate(counter)
+            
+        for param in optimizer.param_groups:
+            param['lr'] = lr
+
+        x, y = get_batch(train_idx, batch_config)
+        optimizer.zero_grad()
+        y_pred = model(x)
+        y_pred = y_pred.view(-1, y_pred.size(-1))
+        loss = criterion(y_pred, y.view(-1))
+
+        print("IDX: {}, loss: {}, lr: {}".format(counter, loss.item(), lr))
+        loss.backward()
+        optimizer.step()
+
+        if counter and counter%testing_interval== 0:
+            checkpoint = model.state_dict()
+
+            model.eval()
+            test_loss = test(model, test_idx, batch_config)
+            model.train()
+            print('test loss', test_loss)
+
+            if test_loss < best_loss:
+                print('Improved best loss. Saving checkpoint')
+                best_loss = test_loss
+                torch.save(checkpoint, 'checkpoint.pt')
+
+            out = model.generate(prompt_tokens, 100, char2idx, idx2char)
+            print(out)
+
+        counter += 1
+
+if __name__ == '__main__':
+    fire.Fire(main)
